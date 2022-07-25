ee78e28383209cb2e8fe82a8a07f94d2aef14eff
Rathod11jay <darji121jay@gmail.com>
2022-07-25 19:43:23 +0530

    25 jul 2022

---
+++ Project3/Project3.js
@@ -0,0 +1,26 @@
+console.log("Welcome To News Website!");
+// News API Key : f9d8a766db174013ad2c4a83a8950088
+
+let getNews = document.getElementById("getNews");
+getNews.addEventListener("click",getNewsHandler);
+
+function getNewsHandler(){
+    console.log("Get News");
+
+    const xhr = new XMLHttpRequest();
+    xhr.open("GET","https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f9d8a766db174013ad2c4a83a8950088",true);
+
+    xhr.onload = function(){
+        if(this.status === 200){
+            let data = JSON.parse(this.responseText);
+            for (let index = 0; index < 10; index++) {
+                console.log(data.articles[index].description);
+                
+            }            
+        }
+        else{
+            console.log("Some Error");
+        }
+    };
+    xhr.send();
+}
