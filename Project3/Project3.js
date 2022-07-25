3528994d3d1a43da7dace3bbff0f44cf216eb02b
Rathod11jay <darji121jay@gmail.com>
2022-07-25 20:22:35 +0530

    25 jul 2022

--- Project3/Project3.js
+++ Project3/Project3.js
@@ -2,25 +2,31 @@ console.log("Welcome To News Website!");
 // News API Key : f9d8a766db174013ad2c4a83a8950088
 
 let getNews = document.getElementById("getNews");
-getNews.addEventListener("click",getNewsHandler);
+getNews.addEventListener("click",getdata);
 
-function getNewsHandler(){
-    console.log("Get News");
-
-    const xhr = new XMLHttpRequest();
-    xhr.open("GET","https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f9d8a766db174013ad2c4a83a8950088",true);
-
-    xhr.onload = function(){
-        if(this.status === 200){
-            let data = JSON.parse(this.responseText);
-            for (let index = 0; index < 10; index++) {
-                console.log(data.articles[index].description);
-                
-            }            
-        }
-        else{
-            console.log("Some Error");
-        }
-    };
-    xhr.send();
+function getdata(){
+    url = "https://newsapi.org/v2/top-headlines?country=in&apiKey=f9d8a766db174013ad2c4a83a8950088";
+    fetch(url).then(response => response.json()).then(data => console.log(data));
 }
+getdata();
+
+// function getNewsHandler(){
+//     console.log("Get News");
+
+//     const xhr = new XMLHttpRequest();
+//     xhr.open("GET","https://newsapi.org/v2/top-headlines?sources=bbc-news&apiKey=f9d8a766db174013ad2c4a83a8950088",true);
+
+//     xhr.onload = function(){
+//         if(this.status === 200){
+//             let data = JSON.parse(this.responseText);
+//             for (let index = 0; index < 10; index++) {
+//                 console.log(data.articles[index].description);
+                
+//             }            
+//         }
+//         else{
+//             console.log("Some Error");
+//         }
+//     };
+//     xhr.send();
+// }
